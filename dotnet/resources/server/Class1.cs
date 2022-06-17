3d1cc30d42da81c248b6db9416d8ff3aabf26114
Eremey Malinovsky <Elerphore@gmail.com>
2022-06-17 23:33:13 +0500

    feat(): Server package initialization

---
+++ dotnet/resources/server/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace server
+{
+    public class Class1 : Script
+    {
+        public Class1() {
+            Console.WriteLine("Hi there");
+        }
+    }
+}
