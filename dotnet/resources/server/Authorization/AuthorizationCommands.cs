872d916e8869c2cf23fdc0cb570a9fbab79e4ac7
Eremey Malinovsky <Elerphore@gmail.com>
2022-06-19 03:51:58 +0500

    refactiong(): Project structure refactoring

--- dotnet/resources/server/Authorization/AuthorizationCommands.cs
+++ dotnet/resources/server/Authorization/AuthorizationCommands.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using GTANetworkAPI;
 using GTANetworkMethods;
+using server.Database;
 using Player = GTANetworkAPI.Player;
+using Task = System.Threading.Tasks.Task;
 
 namespace server.Authorization {
     public class AuthorizationCommands : Script {
         [Command(command: "reg", Description = "Register new account")]
-        public void OnRegistration(Player player, string login, string email, string password) {
-            NAPI.Util.ConsoleOutput("reg");
+        public async Task OnRegistration(Player player, string login, string email, string password) {
+            try {
+                await DatabaseManager.RegisterPlayer(login, email, password);
+            }
+            catch (Exception e) {
+                NAPI.Util.ConsoleOutput("err: " + e);
+            }
+
         }
 
         [Command(command: "log", Description = "Login into account")]
--- dotnet/resources/server/Database/DatabaseManager.cs
+++ dotnet/resources/server/Database/DatabaseManager.cs
@@ -1,5 +1,29 @@
-﻿namespace server.Database {
-    public class DatabaseManager {
-        
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using GTANetworkAPI;
+using Npgsql;
+
+namespace server.Database {
+    public static class DatabaseManager {
+        private const string ConnString = "Host=127.0.0.1;Username=postgres;Password=postgres;Database=luny";
+        private static readonly NpgsqlConnection Conn = new NpgsqlConnection(ConnString);
+
+        public static async Task RegisterPlayer(string login, string email, string password) {
+            try {
+                await Conn.OpenAsync();
+
+            await using var cmd = new NpgsqlCommand("INSERT INTO players (login, email, password) VALUES ($1, $2, $3)", Conn);
+            cmd.Parameters.AddWithValue(login);
+            cmd.Parameters.AddWithValue(email);
+            cmd.Parameters.AddWithValue(password);
+            await cmd.ExecuteNonQueryAsync();
+
+            await Conn.CloseAsync();
+            } catch(Exception e) {
+                NAPI.Util.ConsoleOutput("err: " + e);
+            }
+
+        }
     }
 }
